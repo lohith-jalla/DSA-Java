9dac847d444ecc9c22a3a2ba96910d0a0ed67005
lohith-jalla <lohithjalla12@gmail.com>
2024-06-13 19:03:01 +0530

    Rectified the mistakes and commited and trying for jenkins.

--- src/Sample_classForJUnitTest.java
+++ src/Sample_classForJUnitTest.java
@@ -3,4 +3,8 @@ public class Sample_classForJUnitTest {
     public int add(int a,int b){
         return a+b;
     }
+    public static void main(String[] args) {
+        Sample_classForJUnitTest a= new Sample_classForJUnitTest();
+        System.out.println(a.add(2,3));
+    }
 }
